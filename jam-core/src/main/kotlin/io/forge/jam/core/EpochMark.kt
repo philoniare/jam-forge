4ca427fdef69437090203d23f97dd4aedff726fb
philoniare <philoniare@gmail.com>
2024-11-24 21:03:50 +0800

    feat: add tickets_entropy to header epoch_mark

--- jam-core/src/main/kotlin/io/forge/jam/core/EpochMark.kt
+++ jam-core/src/main/kotlin/io/forge/jam/core/EpochMark.kt
@@ -2,18 +2,23 @@ package io.forge.jam.core
 
 import io.forge.jam.core.serializers.ByteArrayHexSerializer
 import io.forge.jam.core.serializers.ByteArrayListHexSerializer
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class EpochMark(
     @Serializable(with = ByteArrayHexSerializer::class)
     val entropy: ByteArray,
+    @Serializable(with = ByteArrayHexSerializer::class)
+    @SerialName("tickets_entropy")
+    val ticketsEntropy: ByteArray,
     @Serializable(with = ByteArrayListHexSerializer::class)
     val validators: List<ByteArray>
 ) : Encodable {
     override fun toString(): String {
         return "EpochMark(" +
             "entropy=${entropy.toHex()}, " +
+            "ticketsEntropy=${ticketsEntropy.toHex()}, " +
             "validators=[${validators.joinToString(",") { it.toHex() }}]" +
             ")"
     }
@@ -23,19 +28,21 @@ data class EpochMark(
             validators.fold(byteArrayOf()) { acc, validator ->
                 acc + validator
             }
-        return entropy + validatorsBytes
+        return entropy + ticketsEntropy + validatorsBytes
     }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is EpochMark) return false
         return entropy.contentEquals(other.entropy) &&
+            ticketsEntropy.contentEquals(other.ticketsEntropy) &&
             validators.size == other.validators.size &&
             validators.zip(other.validators).all { (a, b) -> a.contentEquals(b) }
     }
 
     override fun hashCode(): Int {
         var result = entropy.contentHashCode()
+        result = 31 * result + ticketsEntropy.contentHashCode()
         result = 31 * result + validators.fold(0) { acc, bytes ->
             31 * acc + bytes.contentHashCode()
         }
